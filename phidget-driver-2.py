46e47a8fbded6f9ed5304c86effee044f4b3f4f9
rom <lsdark73@gmail.com>
2010-03-02 05:35:30 +0100

    Ajout d'init d'un phidget Ajout de handlers pour attach/detach phidget

--- phidget-driver-2.py
+++ phidget-driver-2.py
@@ -5,6 +5,17 @@
 #
 #
 
+# Generic
+import logging
+
+
+# DBUS
+import gobject
+import dbus
+import dbus.service
+import dbus.glib
+
+
 # Phidget specific imports
 from Phidgets.PhidgetException import *
 from Phidgets.Events.Events import *
@@ -19,14 +30,35 @@ class PhidgetInterface(dbus.service.Object):
         Accès aux interfaces Phidgets
     """
     
-    def __init__(self, serial, index):
+    def __init__(self, name, serial):
         
+        self.name = name
         self.serial = serial
-        self.index = index
-        self.path
-        
+
+        self.logfile = "phidgets.log"
+
         bus_name = dbus.service.BusName('org.openplacos.drivers', bus = dbus.SessionBus())
         dbus.service.Object.__init__(self, bus_name, '/org/openplacos/drivers/phidgets/' )
 
         # Attache de l'interface
+        self.interface = InterfaceKit()
+        self.interface.enableLogging(PhidgetLogLevel.PHIDGET_LOG_VERBOSE, self.logfile)
         
+        self.interface.setOnAttachHandler(self.phidget_attach_handler)
+        self.interface.setOnDetachHandler(self.phidget_detach_handler)
+        self.interface.openPhidget(self.serial)
+        
+        
+    ##
+    ## Phidgets Attach/Detach handlers
+    ##
+    def phidget_attach_handler(self, e):
+        """ Handler de phidget attaché """
+        attached = e.device
+        logging.debug("Attach %s : %s" % (attached.getDeviceType(), attached.getSerialNum() ) )
+    
+    def phidget_detach_handler(self, e):
+        """ Handler de phidget détaché """
+        detached = e.device
+        logging.debug("Detach %s : %s" % (detached.getDeviceType(), detached.getSerialNum() ) )
+
