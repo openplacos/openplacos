efa4d86bbf096c6076dec1382f7a12fda46d74a8
rom <lsdark73@gmail.com>
2010-03-02 14:02:47 +0100

    Ajout d'une méthode Manager pour tester les phidgets physiquement branchés

--- phidget-driver-2.py
+++ phidget-driver-2.py
@@ -7,7 +7,7 @@
 
 # Generic
 import logging
-
+import time
 
 # DBUS
 import gobject
@@ -17,6 +17,7 @@ import dbus.glib
 
 
 # Phidget specific imports
+from Phidgets.Phidget import PhidgetLogLevel
 from Phidgets.PhidgetException import *
 from Phidgets.Events.Events import *
 from Phidgets.Manager import *
@@ -37,17 +38,45 @@ class PhidgetInterface(dbus.service.Object):
 
         self.logfile = "phidgets.log"
 
-        bus_name = dbus.service.BusName('org.openplacos.drivers', bus = dbus.SessionBus())
-        dbus.service.Object.__init__(self, bus_name, '/org/openplacos/drivers/phidgets/' )
+        bus_name = dbus.service.BusName('org.openplacos.drivers.phidgets', bus = dbus.SessionBus())
+        dbus.service.Object.__init__(self, bus_name, '/org/openplacos/drivers/phidgets' )
 
-        # Attache de l'interface
-        self.interface = InterfaceKit()
-        self.interface.enableLogging(PhidgetLogLevel.PHIDGET_LOG_VERBOSE, self.logfile)
+        # Attache de(s) l'interface
+        self.devices = []
         
-        self.interface.setOnAttachHandler(self.phidget_attach_handler)
-        self.interface.setOnDetachHandler(self.phidget_detach_handler)
-        self.interface.openPhidget(self.serial)
-        
+        phidget = InterfaceKit()
+        phidget.enableLogging(PhidgetLogLevel.PHIDGET_LOG_VERBOSE, self.logfile)
+        phidget.setOnAttachHandler(self.phidget_attach_handler)
+        phidget.setOnDetachHandler(self.phidget_detach_handler)
+        phidget.openPhidget(self.serial)
+        try:
+            phidget.waitForAttach(2000)
+        except PhidgetException as e:
+            print("Phidget Exception %i: %s" % (e.code, e.details))
+            print("Exiting....")
+            exit(1)
+            
+        self.devices.append(phidget)
+    
+    def manager(self):
+        """
+            Cette méthode permet d'interroger les phidgets connectés 
+        """
+        # on commence par fermer tous les phidgets ouverts
+        #for phidget in self.devices:
+        #    if phidget.isAttached(): phidget.closePhidget()
+        mgr = Manager()
+        mgr.openManager()
+        attachedDevices = mgr.getAttachedDevices()
+        print("|------------|----------------------------------|--------------|------------|")
+        print("|- Attached -|-              Type              -|- Serial No. -|-  Version -|")
+        print("|------------|----------------------------------|--------------|------------|")
+        for attachedDevice in attachedDevices:
+            print("|- %8s -|- %30s -|- %10d -|- %8d -|" % (attachedDevice.isAttached(), attachedDevice.getDeviceType(), attachedDevice.getSerialNum(), attachedDevice.getDeviceVersion()))
+
+        print("|------------|----------------------------------|--------------|------------|")
+
+    
         
     ##
     ## Phidgets Attach/Detach handlers
@@ -55,10 +84,21 @@ class PhidgetInterface(dbus.service.Object):
     def phidget_attach_handler(self, e):
         """ Handler de phidget attaché """
         attached = e.device
-        logging.debug("Attach %s : %s" % (attached.getDeviceType(), attached.getSerialNum() ) )
+        print ("Attach %s : %s" % (attached.getDeviceType(), attached.getSerialNum() ) )
     
     def phidget_detach_handler(self, e):
         """ Handler de phidget détaché """
         detached = e.device
-        logging.debug("Detach %s : %s" % (detached.getDeviceType(), detached.getSerialNum() ) )
+        print ("Detach %s : %s" % (detached.getDeviceType(), detached.getSerialNum() ) )
 
+
+
+
+## En live..
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.DEBUG)
+    phidgets = PhidgetInterface("8/8/8", 77225) 
+    phidgets.manager()
+   
+    
