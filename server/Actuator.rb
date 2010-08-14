71c61f8277ab0447c36120a2eb4bae46920fd841
flagos <flagospub@gmail.com>
2010-08-14 12:52:30 +0200

    parse config in actuator modified

--- server/Actuator.rb
+++ server/Actuator.rb
@@ -30,7 +30,7 @@ class Actuator
     @room = nil
     @state = -1
     @device_model = nil
- 
+    
     #detect model and merge with config
     if act_["model"]
       #parse yaml
@@ -71,75 +71,77 @@ class Actuator
   
   def parse_config(config_)
     #parse config and add variable according to the config and the model
-    
+
+    #Create hashes
+    @methods = Hash.new    
+
+
+    # Error processing
+    if config_["driver"]["interface"].nil?
+      abort "Error in model " + model["model"] + " : interface is required "
+    end
+    if config_["name"].nil?
+      abort "Error in config : name is required "
+    end
+
     #for each keys of config
-    config_.each {|key, param| 
-      
-      case key
-      when "room"
-        @room = param
+    @room         = config_["room"]
+    @name         = config_["name"]
+    @device_model = config_["model"]
+    @interface    = Dbus_interface.new(config_["driver"]["interface"]).dup
+    
 
-      when "name"
-        @name = param
+    # Get method defined in model
+    config_["methods"].each { |method|
+      #Check if value is defined for the method
+      #value is required
+      if not method["value"].nil?
+        value = method["value"]
+      else
+        abort "Error in model " + config_["model"] + " : value is required for method " +  method["name"]
+      end
 
-      when "model"
-        @device_model = param
+      #Check if option is defined
+      if method["option"]
+        #Parse option define in yaml to a hash
+        option = method["option"].inspect
+      else
+        #if no option is defined, send an empty hash
+        option = "{}"
+      end
 
-      when "driver"
-        if param["option"]
-          @option = value["option"].dup
-        else
-          @option = Hash.new
-        end
-        
-        if param["interface"]
-          @interface = Dbus_interface.new(param["interface"]).dup
-        else
-          abort "Error in model " + model["model"] + " : interface is required "
-        end
-        
-      when "methods"
-        @methods = Hash.new
-        param.each{ |method|
-          #Check if value is defined for the method
-          #value is required
-          if not method["value"].nil?
-            value = method["value"]
-          else
-            abort "Error in model " + config_["model"] + " : value is required for method " +  method["name"]
-          end
-
-          #Check if option is defined
-          if method["option"]
-            #Parse option define in yaml to a hash
-            option = method["option"].inspect
-          else
-            #if no option is defined, send an empty hash
-            option = "{}"
-          end
-
-          methdef = """
+      methdef = """
           def #{method["name"]}
             write( #{value}, #{option})
-          end
-          """
+                end
+        """
           self.instance_eval(methdef)
           @methods[method["name"]] = method["name"]
         }
-      end
+
+
+    config_.each { |key, param| 
       
+      case key
+      when "driver"
+        if param["option"]
+          @option = value["option"].dup  ### What is value ?
+        else
+          @option = Hash.new
+        end
+      end
     }
   end
   
   def write( value_, option_)
-   @proxy_iface.write( value_, option_)
-   Thread.new{ 
-        flow = Database::Flow.create(:date  => Time.new,:value => to_float(value_)) 
-        device =  Database::Device.find(:first, :conditions => { :config_name => self.name })
-        actuator =  Database::Actuator.find(:first, :conditions => { :device_id => device.id })
-        Database::Instruction.create(:flow_id => flow.id,
-                                 :actuator_id => actuator.id)
-   }
+    @proxy_iface.write( value_, option_)
+    Thread.new{ 
+      flow = Database::Flow.create(:date  => Time.new,:value => to_float(value_)) 
+      device =  Database::Device.find(:first, :conditions => { :config_name => self.name })
+      actuator =  Database::Actuator.find(:first, :conditions => { :device_id => device.id })
+      Database::Instruction.create(:flow_id => flow.id,
+                                   :actuator_id => actuator.id)
+    }
     @state = value_
   end
   
