d6ebee891bc7b6ecb5aa32f7cee4de415734ce1a
flagos <flagospub@gmail.com>
2010-10-25 22:50:56 +0200

    init.d file copy

--- setup.sh
+++ setup.sh
@@ -5,4 +5,5 @@ cp -rf ../openplacos/ /usr/lib/
 ln -s /usr/lib/openplacos/server/Top.rb /usr/bin/openplacos-server
 cp server/config_with_VirtualPlacos.yaml /etc/default/openplacos
 cp setup_files/org.openplacos.drivers.virtualplacos.service /usr/share/dbus-1/services/
+cp setup_files/openplacos /etc/init.d/
 update-rc.d openplacos defaults 98 02
