0fcc4b566331e6dc74c873c81da745416b880d5f
miaoufkirsh <miaoufkirsh@gmail.com>
2011-02-12 16:19:24 +0100

    actuator in rooms view

--- client/rorplacos/app/models/actuator.rb
+++ client/rorplacos/app/models/actuator.rb
@@ -1,9 +1,11 @@
 class Actuator
 
-    attr_reader :path, :state, :methods
+    attr_reader :path, :state, :backend, :node
 
     def initialize(node)
         @path = node.path
+        @node = node
+        @backend = node['org.openplacos.server.actuator']
         # @state = node.object['org.openplacos.server.actuator'].state[0]['name']
         # @state ||= "unknown"
         # @methods = node.object['org.openplacos.server.actuator'].methods.keys
