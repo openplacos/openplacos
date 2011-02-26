21334ff3149fa7b2debcfdda21a8cdff22d35ad3
miaoufkirsh <miaoufkirsh@gmail.com>
2011-02-26 16:34:37 +0100

    remove require dbus in soap plugin

--- server/plugins/soap.rb
+++ server/plugins/soap.rb
@@ -15,7 +15,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with Openplacos.  If not, see <http://www.gnu.org/licenses/>.
 require 'rubygems'
-require 'dbus-openplacos'
 require "soap/rpc/standaloneServer"
 require "openplacos"
 
