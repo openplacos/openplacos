437146ddad004c660bae9b668432b5a1358a3c59
miaoufkirsh <miaoufkirsh@gmail.com>
2011-02-27 20:11:59 +0100

    cleaning code and add CSS

--- client/rorplacos/app/models/actuator.rb
+++ client/rorplacos/app/models/actuator.rb
@@ -11,4 +11,19 @@ class Actuator
         # @methods = node.object['org.openplacos.server.actuator'].methods.keys
         # @methods.delete('state')
     end
+    
+    def state
+      s = @backend.state[0]["name"]
+      if s.nil?
+        s = "NA"
+      end
+      return s
+    end
+    
+    def get_methods
+      meth = @backend.methods.keys
+      meth.delete("state")
+      return meth
+    end
+    
 end
