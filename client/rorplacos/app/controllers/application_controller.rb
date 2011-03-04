552dafa1eee56bab750b5ee9a307c00f2b2c4f59
miaoufkirsh <miaoufkirsh@gmail.com>
2011-03-04 13:24:43 +0100

    add translation system

--- client/rorplacos/app/controllers/application_controller.rb
+++ client/rorplacos/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :set_locale
+  def set_locale
+    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
+    I18n.locale = extract_locale_from_accept_language_header
+    logger.debug "* Locale set to '#{I18n.locale}'"
+  end
+  private
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+
 end
