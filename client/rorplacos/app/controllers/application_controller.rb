9103b9560992c9b3ca4ac118e0462d38ce222591
miaoufkirsh <miaoufkirsh@gmail.com>
2011-03-05 00:31:59 +0100

    top menu

--- client/rorplacos/app/controllers/application_controller.rb
+++ client/rorplacos/app/controllers/application_controller.rb
@@ -3,14 +3,17 @@ class ApplicationController < ActionController::Base
   
   before_filter :set_locale, :set_style
   def set_locale
-    logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
-    I18n.locale = extract_locale_from_accept_language_header
-    logger.debug "* Locale set to '#{I18n.locale}'"
+    if params[:locale]
+      I18n.locale=params[:locale]
+    else
+      I18n.locale = extract_locale_from_accept_language_header
+    end
   end
   private
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
   end
+  
   def set_style
   @style = params[:style] || "style.css"
   end
--- client/rorplacos/app/helpers/application_helper.rb
+++ client/rorplacos/app/helpers/application_helper.rb
@@ -2,12 +2,13 @@ module ApplicationHelper
 
   require 'dbus-openplacos'
 
-  def room_tree(room)
+  def room_tree(room,current)
       room_name = room.path.split("/").pop || "/"
+      room_name = "<div class='current_room'>#{room_name}</div>" if current==room.path
       str = "<li><a href='/opos#{room.path}'>#{room_name}</a></li>\n"
       room.childs.each{ |child|
         str << "<ul>"
-        str << room_tree(child)
+        str << room_tree(child,current)
         str << "</ul>\n"
       }
       #str << "</li>\n"
