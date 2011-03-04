e5540dc62c428b7b4eaa164b27825c6cf7140057
miaoufkirsh <miaoufkirsh@gmail.com>
2011-03-04 16:03:50 +0100

    add static tree to the left menu

--- client/rorplacos/app/helpers/application_helper.rb
+++ client/rorplacos/app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
 
-require 'dbus-openplacos'
+  require 'dbus-openplacos'
+
+  def room_tree(room)
+      room_name = room.path.split("/").pop || "/"
+      str = "<li><a href='/opos#{room.path}'>#{room_name}</a></li>\n"
+      room.childs.each{ |child|
+        str << "<ul>"
+        str << room_tree(child)
+        str << "</ul>\n"
+      }
+      #str << "</li>\n"
+      return str  
+  end
 
 end
