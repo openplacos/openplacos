45c128cd7b422f83a4eb509a620fa52465eb2828
miaoufkirsh <miaoufkirsh@gmail.com>
2011-03-04 13:59:14 +0100

    sylesheet can be set as parameter ?style=stylesheet.css

--- client/rorplacos/app/controllers/application_controller.rb
+++ client/rorplacos/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  before_filter :set_locale
+  before_filter :set_locale, :set_style
   def set_locale
     logger.debug "* Accept-Language: #{request.env['HTTP_ACCEPT_LANGUAGE']}"
     I18n.locale = extract_locale_from_accept_language_header
@@ -10,6 +10,9 @@ class ApplicationController < ActionController::Base
   private
   def extract_locale_from_accept_language_header
     request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+  end
+  def set_style
+  @style = params[:style] || "style.css"
   end
 
 end
