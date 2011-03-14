207dc8d0cbf97082aa72ec988ba55f7d0c1cdd15
flagos <flagospub@gmail.com>
2011-03-14 18:18:15 +0100

    fix warning

--- server/Publish.rb
+++ server/Publish.rb
@@ -1,4 +1,4 @@
-3#    This file is part of Openplacos.
+#    This file is part of Openplacos.
 #
 #    Openplacos is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
