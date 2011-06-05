8f6e600e09be31b3439fed27913c207cc6bb3269
miaoufkirsh <miaoufkirsh@gmail.com>
2011-06-05 18:45:18 +0200

    add gravatar support in profile

--- server/plugins/rorplacos/app/controllers/users_controller.rb
+++ server/plugins/rorplacos/app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
+require 'digest/md5'
+
 class UsersController < ApplicationController
   before_filter :authenticated, :except => :auth
 
   def index
 
     @user = User.find(session[:user_id])
-   
+
+    # get the email from URL-parameters or what have you and make lowercase
+    email_address = @user.email || "default"
+
+    # create the md5 hash
+    hash = Digest::MD5.hexdigest(email_address.downcase)
+
+    # compile URL which can be used in <img src="RIGHT_HERE"...
+    @avatar = "http://www.gravatar.com/avatar/#{hash}?d=mm"
+
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @user }
