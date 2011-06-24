fec2c7bdfd8cb925872cca19254ecf347eb6ad7c
miaoufkirsh <miaoufkirsh@gmail.com>
2011-06-24 22:52:37 +0200

    driver for weather informations

---
+++ drivers/weather/install_dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+#    This file is part of Openplacos.
+#
+#    Openplacos is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Openplacos is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Openplacos.  If not, see <http://www.gnu.org/licenses/>.
+
+if [ "$(id -u)" != "0" ]; then
+	echo "Root permission needed"
+	echo "Syntax: sudo $0"
+	exit 1
+fi
+
+gem install barometer --no-ri --no-rdoc
