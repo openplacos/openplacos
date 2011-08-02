aa59e9e373bab55aaa63ebd5cb89c9ec5566e56a
flagos <flagospub@gmail.com>
2011-08-02 23:29:14 +0200

    git add Info.rb

---
+++ server/Info.rb
@@ -0,0 +1,33 @@
+#    This file is part of Openplacos.
+#
+#    Openplacos is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Openplacos is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Openplacos.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class Info < DBus::Object
+
+  def initialize()
+    super("/informations")
+  end 
+
+  dbus_interface "org.openplacos.server.informations" do 
+    dbus_method :get_internal_dbus_address, "out address:s" do
+      return ENV["DBUS_SESSION_BUS_ADDRESS"]
+    end
+  end
+
+
+end
+
+  
