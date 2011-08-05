d52f27ab888f85c18e044dd5b21a874cc294c911
miaoufkirsh <miaoufkirsh@gmail.com>
2011-08-05 18:54:35 +0200

    add arduino binaries and script for uploading

---
+++ drivers/Arduino/firmware/binaries/upload.sh
@@ -0,0 +1,20 @@
+echo "Arduino board ?"
+echo "Uno      [1]"
+echo "Mega1280 [2]"
+echo "Mega2560 [3]"
+read board
+
+case $board in
+  1) chip="ATMEGA328P";file="Uno.hex";;
+  2) chip="ATMEGA1280";file="MEGA1280.hex";;
+  3) chip="ATMEGA2560";file="MEGA2560.hex";;
+esac
+
+echo $chip
+echo $file
+
+echo "USB port ?"
+read port
+
+echo "Start uploading"
+avrdude -F -V -c arduino -p $chip -P $port -b 115200 -U flash:w:$file 
