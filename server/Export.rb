0d18fc4dc35496f0db240776d7eca6bb35c0fe93
miaoufkirsh <miaoufkirsh@gmail.com>
2012-01-31 19:06:56 +0100

    ressource prefix for uri

--- server/Export.rb
+++ server/Export.rb
@@ -38,17 +38,17 @@ class Export
       outHash["interfaces"] << pin.config.keys # get the ifaces and push it in the hash
     end
     outHash["interfaces"].flatten!
-    WebServer.instance_eval( "get('#{@path_dbus}') {'#{outHash.to_json}'}")
+    WebServer.instance_eval( "get('/ressources#{@path_dbus}') {'#{outHash.to_json}'}")
     
-    # generate uri and responce for iface
+    # generate uri and response for iface
     pin_plugs.each do |pin|
       outHash = Hash.new
       outHash["name"] = @path_dbus
       pin.config.each do |iface, meths|
         outHashbis = outHash.dup
         outHashbis["interface"] = iface.to_s
-        if meths.include?("read") # read correspond to get
-          WebServer.instance_eval( "get('#{@path_dbus}/#{iface.to_s}') { #{outHashbis.inspect}.merge({'value' => Dispatcher.instance.call('#{@path_dbus}','#{iface}','read',{})[0]}).to_json }")
+        if meths.include?("read") # read corresponds to get
+          WebServer.instance_eval( "get('/ressources#{@path_dbus}/#{iface.to_s}') { #{outHashbis.inspect}.merge({'value' => Dispatcher.instance.call('#{@path_dbus}','#{iface}','read',{})[0]}).to_json }")
         end
       end
 
