4f3cb2b82e99d05c300fc6edc34383d3b11f65b0
flagos <flagospub@gmail.com>
2012-02-02 20:13:27 +0100

    require in migrate

--- server/migrate.rb
+++ server/migrate.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/ruby
 
+require "rubygems"
 require "active_record"
+require "oauth2/provider"
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "test.db", :pool => 25)
 ActiveRecord::Migrator.migrate('db')
