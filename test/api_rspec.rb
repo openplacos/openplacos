dca501bbb0120343e464711b69f589786b67f336
miaoufkirsh <miaoufkirsh@gmail.com>
2012-04-17 22:02:04 +0200

    api test

---
+++ test/api_rspec.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+require './server.rb'
+
+CONFIG_FILE = "../config/default.yaml"
+
+describe Server, "#api" do
+  
+  before(:all) do
+    @server = Server.new("-f #{CONFIG_FILE} -s")
+    @server.launch
+    @server.wait_launch
+    
+    @config = YAML::load(File.read(CONFIG_FILE))
+  end
+  
+  it "ressources names should equal to thoses declared in config file" do
+    @server.ressources.keys.should eq(@config["export"])
+  end
+  
+  after(:all) do
+    @server.kill
+  end
+  
+end
+
+
