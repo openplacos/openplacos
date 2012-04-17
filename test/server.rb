c15cd3bf032ff936292ce26b7b78bb4649a1dce0
miaoufkirsh <miaoufkirsh@gmail.com>
2012-04-17 21:53:12 +0200

    test get ressources

--- test/server.rb
+++ test/server.rb
@@ -1,31 +1,57 @@
 require 'net/http'
-require 'uri'
+require 'json'
+
 class Server
   def initialize(str_)
     @arg = str_
     @status = false
   end 
   
-  #launch the server
+  # Launch the server
   def launch
     @th = Thread.new do
       @status = system "dbus-launch ../server/main.rb " + @arg 
     end
   end
   
-  # check if the server is launched by trying to connect
+  # Kill the server
+  def kill
+    system "pkill main.rb"        
+  end
+  
+  # wait for the complete launch of the server
+  def wait_launch
+    begin
+      url = URI.parse('http://localhost:4567')
+      res = Net::HTTP.start(url.host, url.port) 
+    rescue Errno::ECONNREFUSED
+      sleep 1
+      retry
+    end
+    return true
+  end
+
+  # check if the server is launched by trying to connect and kill it
   def launched?
     th = Thread.new do
-      begin
-        url = URI.parse('http://localhost:4567')
-        res = Net::HTTP.start(url.host, url.port) 
-        system "pkill main.rb"        
-      rescue Errno::ECONNREFUSED
-        sleep 1
-        retry
-      end
+      wait_launch
+      kill
     end
     @th.join
     return @status
   end
+  
+  def get(url)
+    JSON.parse Net::HTTP.get(URI.parse("http://localhost:4567#{url}"))
+  end
+  
+  def ressources
+    res = get('/ressources')
+    ressources = Hash.new
+    res.each do |res|
+      ressources[res["name"]] = res
+    end
+    return ressources
+  end
+  
 end
