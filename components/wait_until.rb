b77f48f4bab5ac78ae839a1b02dc1bac7d248750
flagos <flagospub@gmail.com>
2012-07-08 11:02:42 +0200

    add of wait_until component

---
+++ components/wait_until.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) << "/LibComponent.rb"
+
+component = LibComponent::Component.new(ARGV) do |c|
+  c.description  "Wait for an event in pulling mode"
+  c.version "0.1"
+  c.default_name "relaync"
+#  c.frequency "Pulling frequency in second"
+end
+
+component << Sensor   = LibComponent::Output.new("/sensor","digital")
+component << Actuator = LibComponent::Output.new("/actuator","digital")
+component << Switch   = LibComponent::Input.new("/switch","digital.order.switch")
+
+Switch.on_write do |value, option|
+  if value==1 or value==true
+    @state = true
+    @thread = Thread.new {
+      while Sensor.read({}) == 0 do
+        Actuator.write(true,option)
+        sleep 1
+      end
+      Actuator.write(false,option)
+      @state.false
+    }
+    sleep 3
+    return true
+  elsif value==0 or value==false
+    @state = false
+    if @thread.alive?
+      @thread.kill
+    end
+    return Raw.write(false,option)
+  end
+end
+
+Switch.on_read do |option|
+  return @state || false
+end
+
+component.run
